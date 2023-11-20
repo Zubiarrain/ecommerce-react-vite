1d6c3aec9410884d3e6a392eda32f76ceb89d4b0
Zubiarrain <zubiarrainnahuel@gmail.com>
2023-11-20 15:30:12 -0300

    fix(vite.config): base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://Zubiarrain.github.io/ecommerce-react-vite"
+  base:"/ecommerce-react-vite/"
 })
